ee8cd2d3284e50531f6579e1be18737ebab65b7e
Jorge <jorgecolamarco03@gmail.com>
2026-05-27 17:44:46 -0500

    Commit 1 clase 27-05-2026

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+import { Grid } from '@mui/material';
+
+
+function App() {
+  return (
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }}
+        sx={{ display: { xs: "none", md: "block" } }} >
+        Elemento: Alertas</Grid>
+
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Selector</Grid>
+
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Indicadores</Grid>
+
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, md: 12 }}
+        sx={{ display: { xs: "none", md: "block" } }} >
+        Elemento: Gráfico</Grid>
+
+      {/* Tabla */}
+      <Grid size={{ xs: 12, md: 12 }}
+        sx={{ display: { xs: "none", md: "block" } }} >
+        Elemento: Tabla</Grid>
+
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+    </Grid>
+  );
+}
+export default App;
+
